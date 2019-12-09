edc192d9dea8701f42bef4223d764bddd7c4346f
Bryan Fernandez <bryfern710@gmail.com>
2019-12-09 16:49:35 -0600

    fnihsed copyspecial

--- copyspecial.py
+++ copyspecial.py
@@ -9,36 +9,42 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-import re
 import os
 import shutil
 import subprocess
 import argparse
 
-# This is to help coaches and graders identify student assignments
-__author__ = "???"
-
-
-# +++your code here+++
-# Write functions and modify main() to call them
+__author__ = "Bryan"
 
 def main():
-    # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO need an argument to pick up 'from_dir'
+    parser.add_argument('from_dir', help='from directory')
     args = parser.parse_args()
 
-    # TODO you must write your own code to get the cmdline args.
-    # Read the docs and examples for the argparse module about how to do this.
+    file_directory = os.listdir(args.from_dir)
+    
+    if args.todir:     
+        dst_path = os.getcwd() + args.todir
+        src_path = os.getcwd()
+        try:
+            shutil.copytree(src_path, dst_path)
+        except:
+            print("Format: --todir /yourdirectory\nDirectory and/or file(s) May exist already")
 
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation.  If something is wrong (or missing) with any
-    # required args, the general rule is to print a usage message and exit(1).
+        return
 
-    # +++your code here+++
-    # Call your functions
+    elif args.tozip:
+        subprocess.call(['zip', '-r', args.tozip, args.from_dir])
+        print("Command I'm going to do: zip -r {} {}".format(args.tozip, args.from_dir))
+        return
+
+
+    for file in file_directory:
+
+        file_path = os.path.abspath(file)
+        print(file_path)
 
 
 if __name__ == "__main__":
